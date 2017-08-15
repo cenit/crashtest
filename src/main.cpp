8ead430e4eafdd79902ca5d4a7c55afef67fe621
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-08-15 13:43:04 +0200

    tests for identifying grid

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,16 @@
 
 
 #include <iostream>
-#include <opencv2/highgui/highgui.hpp>
+
+//#include "opencv2/cv.h"
+//#include "opencv2/core/core.hpp"
+//#include "opencv2/highgui/highgui.hpp"
+//#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "iostream"
+
 #include <boost/filesystem.hpp>
 
 #include "error_codes.h"
@@ -13,13 +22,14 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-  cout << "crashtest v " << MAJOR_VERSION << '.' << MINOR_VERSION << endl;
+  cout << "crashtest " << MAJOR_VERSION << '.' << MINOR_VERSION << " using OpenCV " << CV_MAJOR_VERSION  << '.' << CV_MINOR_VERSION << endl;
 
   VideoCapture cap;
+  int framecounter = 0; 
 
 #ifdef _WIN32
   std::string filename = "C:\\Users\\sinigard\\Codice\\crashtest\\video\\PrimoCrash.mts";
-  cap.set(CV_CAP_PROP_FOURCC, CV_FOURCC('H', '2', '6', '4'));
+  //OpenCV cannot use ffmpeg in libraries build by vcpkg, so this software still does not work in Windows...
 #else
   std::string filename = "/mnt/c/Users/sinigard/Codice/crashtest/video/PrimoCrash.mts";
 #endif
@@ -40,21 +50,41 @@ int main(int argc, char* argv[])
     cin.get();
     exit(ERROR_VIDEO_NOT_FOUND);
   }
-  //cap.set(CV_CAP_PROP_POS_MSEC, 300);  //start the video at 300ms
+  cap.set(CV_CAP_PROP_POS_MSEC, 44000);  //start the video at 20000ms (20s)
   double fps = cap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
   cout << "Frame per seconds : " << fps << endl;
-  namedWindow("CrashTest_Video", CV_WINDOW_AUTOSIZE);
+  namedWindow("Video", WINDOW_NORMAL);
+  namedWindow("test", WINDOW_NORMAL);
+  namedWindow("Grid", WINDOW_NORMAL);
+  resizeWindow("Video", 640, 360);
+  resizeWindow("test", 640, 360);
+  resizeWindow("Grid", 640, 360);
 
   while (true)
   {
     Mat frame;
+    Mat grid;
     bool bSuccess = cap.read(frame);
     if (!bSuccess)
     {
       cout << "Video is over, exiting..." << endl;
       break;
     }
-    imshow("CrashTest_Video", frame);
+    framecounter++;
+    imshow("Video", frame);
+    cvtColor( frame, frame, COLOR_BGR2GRAY );
+    threshold(frame, grid, 230.0, 255.0, THRESH_BINARY);
+    erode(grid, grid, Mat(), Point(-1, -1), 1, 1, 1);
+    dilate(grid, grid, Mat(), Point(-1, -1), 12, 1, 1);
+    erode(grid, grid, Mat(), Point(-1, -1), 11, 1, 1);
+    bitwise_and(frame, frame, grid, grid);
+    erode(grid, grid, Mat(), Point(-1, -1), 15, 1, 1);
+    dilate(grid, grid, Mat(), Point(-1, -1), 15, 1, 1);
+    imshow("test", grid);
+    threshold(grid, grid, 180.0, 255.0, THRESH_BINARY);
+    printf("Frame #%06d - mask r: %d, c:%d     \t\r", framecounter, grid.rows, grid.cols);
+    imshow("Grid", grid);
+
     if (waitKey(30) == 27)  //wait for 'esc' key press for 30 ms.
     {
       cout << "ESC key is pressed by user, exiting..." << endl;
